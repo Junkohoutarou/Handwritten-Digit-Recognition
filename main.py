7ad056799d08b7097a6ccc049b74b4fc974fcee3
Nguyen Phu Hao <126962580+Junkohoutarou@users.noreply.github.com>
2023-12-08 10:18:40 +0700

    Update main.py

--- main.py
+++ main.py
@@ -3,13 +3,10 @@ from PIL import Image
 import pickle as pkl
 import numpy as np
 
-IMG_SIZE = 227
-
-class_list = {'0': 'NORMAL', '1': 'PNEUMONIA'}
 
 st.title('Pneumonia prediction based on chest X-Ray image')
 
-input = open('lrc_xray.pkl', 'rb')
+input = open('lrc_mnist.pkl', 'rb')
 model = pkl.load(input)
 
 st.header('Upload hardwritten digit image')
@@ -20,9 +17,9 @@ if uploaded_file is not None:
     st.image(image, caption='Test image')
 
     if st.button('Predict'):
-        image = image.resize((IMG_SIZE*IMG_SIZE*3, 1))
+        image = image.resize((8*8, 1))
         feature_vector = np.array(image)
         label = str((model.predict(feature_vector))[0])
 
         st.header('Result')
-        st.text(class_list[label])
+        st.text(label)
