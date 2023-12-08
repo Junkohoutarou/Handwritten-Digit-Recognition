870732a12f9b37c16941d23e75b514e7d1d25b7f
Nguyen Phu Hao <126962580+Junkohoutarou@users.noreply.github.com>
2023-12-08 10:24:19 +0700

    Update main.py

--- main.py
+++ main.py
@@ -3,23 +3,30 @@ from PIL import Image
 import pickle as pkl
 import numpy as np
 
+st.title('Handwritten Digit Recognition')
 
-st.title('Handwritten')
+input_model = open('lrc_mnist.pkl', 'rb')
+model = pkl.load(input_model)
 
-input = open('lrc_mnist.pkl', 'rb')
-model = pkl.load(input)
-
-st.header('Upload hardwritten digit image')
-uploaded_file = st.file_uploader("Choose an image file", type=(['png', 'jpg', 'jpeg']))
+st.header('Upload handwritten digit image')
+uploaded_file = st.file_uploader("Choose an image file", type=['png', 'jpg', 'jpeg'])
 
 if uploaded_file is not None:
     image = Image.open(uploaded_file)
-    st.image(image, caption='Test image')
+    st.image(image, caption='Test image', use_column_width=True)
 
     if st.button('Predict'):
-        image = image.resize((8*8, 1))
-        feature_vector = np.array(image)
-        label = str((model.predict(feature_vector))[0])
+        # Resize image and convert to a flat array
+        image = image.resize((8, 8))  # Adjust the size accordingly
+        feature_vector = np.array(image).flatten()
+
+        # Normalize pixel values to be between 0 and 1 (if needed)
+        feature_vector = feature_vector / 255.0
+
+        # Make prediction
+        label = str((model.predict([feature_vector]))[0])
 
         st.header('Result')
-        st.text(label)
+        st.text(f'Predicted digit: {label}')
+else:
+    st.info('Please upload an image.')
