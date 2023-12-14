5adb2296fb48ba9cb198fc806bde17f92269215f
Nguyen Phu Hao <126962580+Junkohoutarou@users.noreply.github.com>
2023-12-14 23:02:01 +0700

    Update main.py

--- main.py
+++ main.py
@@ -2,3 +2,8 @@ import streamlit as st
 from PIL import Image
 import pickle as pkl
 import numpy as np
+
+st.title('Handwritten Digit Recognition')
+
+input = open('lrc_mnist.pkl', 'rb')
+model = pkl.load(input)
