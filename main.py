8fe3139da019c9aa6673564666216ec9f9fb143e
Nguyen Phu Hao <126962580+Junkohoutarou@users.noreply.github.com>
2023-12-14 23:02:31 +0700

    Update main.py

--- main.py
+++ main.py
@@ -7,3 +7,6 @@ st.title('Handwritten Digit Recognition')
 
 input = open('lrc_mnist.pkl', 'rb')
 model = pkl.load(input)
+
+st.header('Upload handwritten digit image')
+uploaded_file = st.file_uploader("Choose an image file", type=(['png', 'jpg', 'jpeg']))
